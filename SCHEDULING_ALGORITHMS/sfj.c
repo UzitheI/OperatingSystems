d6cc17cf533ee4addfab6dc41b4b030163d00787
UzitheI <ujjwalpuri221@gmail.com>
2024-12-16 04:33:33 +0545

    Program for Shortest Job First

---
+++ SCHEDULING_ALGORITHMS/sfj.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+void findWaitingTime(int n, int bt[], int wt[]) {
+    wt[0] = 0;  
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+}
+
+void findTurnAroundTime(int n, int bt[], int wt[], int tat[]) {
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findAvgTime(int processes[], int n, int bt[]) {
+    int wt[n], tat[n];
+    int total_wt = 0, total_tat = 0;
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (bt[i] > bt[j]) {
+
+                int temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+
+                int tempProcess = processes[i];
+                processes[i] = processes[j];
+                processes[j] = tempProcess;
+            }
+        }
+    }
+
+
+    findWaitingTime(n, bt, wt);
+
+
+    findTurnAroundTime(n, bt, wt, tat);
+
+
+    printf("Processes   Burst Time   Waiting Time   Turnaround Time\n");
+
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("  %d\t\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage waiting time = %.2f", (float)total_wt / (float)n);
+    printf(" \nAverage turnaround time = %.2f\n", (float)total_tat / (float)n);
+}
+
+int main() {
+    int processes[] = {1, 2, 3};
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    int burst_time[] = {24,3,3};
+
+    findAvgTime(processes, n, burst_time);
+
+    return 0;
+}
